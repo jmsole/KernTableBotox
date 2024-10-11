43d424093cae5665bfd06aad64ef3ceb9e501ab0
José Solé <type@jmsole.cl>
2024-10-11 13:43:00 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,38 +1,33 @@
 # -*- coding: utf-8 -*-
 
-from setuptools import find_packages, setup
+from setuptools import setup
 
 
 def readme():
-    with open("README.md") as f:
-        return f.read()
+	with open("README.md") as f:
+		return f.read()
 
 
 setup(
-    name="KernTableBotox",
-    version="0.2",
-    description="a little tool to inject old school kern table in font files",
-    long_description=readme(),
-    classifiers=[
-        "Development Status :: 5 - Beta",
-        "License :: Other/Proprietary License",
-        "Programming Language :: Python :: 3.10.4",
-        "Topic :: Software Development :: Build Tools",
-    ],
-    author="Mathieu Reguer",
-    author_email="mathieu.reguer@gmail.com",
-    license="All rights reserved",
-    packages=find_packages("KernTableBotox"),
-    package_dir={"": "KernTableBotox"},
-    entry_points={
-        "console_scripts": [
-            "KernTableBotox = KernTableBotox.KernTableBotox:inject_kern_table",
-        ],
-    },
-    install_requires=[
-        "fonttools",
-        "click",
-    ],
-    include_package_data=True,
-    zip_safe=False,
+	name="KernTableBotox",
+	version="0.2",
+	description="a little tool to inject old school kern table in font files",
+	long_description=readme(),
+	classifiers=[
+		"Development Status :: 5 - Beta",
+		"License :: Other/Proprietary License",
+		"Programming Language :: Python :: 3.10.4",
+		"Topic :: Software Development :: Build Tools",
+	],
+	author="Mathieu Reguer",
+	author_email="mathieu.reguer@gmail.com",
+	license="All rights reserved",
+	packages={"KernTableBotox"},
+	entry_points={"console_scripts": ["KernTableBotox = KernTableBotox.KernTableBotox:inject_kern_table"]},
+	install_requires=[
+		"fonttools",
+		"click",
+	],
+	include_package_data=True,
+	zip_safe=False,
 )
