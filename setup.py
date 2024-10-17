f00e69481823228cdae910af58151b0691deac8a
José Solé <type@jmsole.cl>
2024-10-17 10:21:20 +0100

    Revert "Update filter list. Add option for minimum kern value to include."
    This reverts commit 9bee6303612869b5393ffb1a25348f42fb3fb9dc.
    
    #Conflicts:
    #	setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from setuptools import setup
 
 
 def readme():
+<<<<<<< HEAD
 	with open("README.md") as f:
 		return f.read()
 
@@ -31,3 +32,35 @@ setup(
 	include_package_data=True,
 	zip_safe=False,
 )
+=======
+  with open("README.md") as f:
+    return f.read()
+
+
+setup(name="KernTableBotox",
+      version="0.1",
+      description="a little tool to inject old school kern table in font files",
+      long_description=readme(),
+      classifiers=[
+          "Development Status :: 4 - Beta",
+          "License :: Other/Proprietary License",
+          "Programming Language :: Python :: 2.7",
+          "Topic :: Software Development :: Build Tools",
+      ],
+      author="Mathieu Reguer",
+      author_email="mathieu.reguer@gmail.com",
+      license="All rights reserved",
+      packages=[
+          "KernTableBotox",
+      ],
+      entry_points="""
+        [console_scripts]
+        KernTableBotox=KernTableBotox.KernTableBotox:inject_kern_table
+        """,
+      install_requires=[
+          "fonttools",
+          "click",
+      ],
+      include_package_data=True,
+      zip_safe=False)
+>>>>>>> parent of 9bee630... Update filter list. Add option for minimum kern value to include.
